1a31021f12f737045c27300b3f18ce053f70231c
Soubhik Mandal <soubhik.mondal.com@gmail.com>
2026-06-01 12:16:48 +0530

    Report_meter_reading

--- electricity-backend/electricity/src/main/java/com/tarifvergleich/electricity/dto/ReportMeterReadingDto.java
+++ electricity-backend/electricity/src/main/java/com/tarifvergleich/electricity/dto/ReportMeterReadingDto.java
@@ -7,6 +7,10 @@ import lombok.Data;
 @Data
 public class ReportMeterReadingDto {
 
+	private Long customerId;
+	
+	private String customerName;
+	
     private Integer deliveryId;
 
     private Integer orderId;
--- electricity-backend/electricity/src/main/java/com/tarifvergleich/electricity/model/ReportMeterReading.java
+++ electricity-backend/electricity/src/main/java/com/tarifvergleich/electricity/model/ReportMeterReading.java
@@ -6,6 +6,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
 
@@ -17,6 +19,10 @@ public class ReportMeterReading {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+    
+    @ManyToOne
+    @JoinColumn(name = "customer_id")
+    private Customer customer;
 
     private Integer deliveryId;
 
